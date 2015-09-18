8fab857b4067e4592a9410685c71752580ea0d6e
Cesar Andreu <cesarandreu@gmail.com>
2015-09-18 11:18:56 -0700

    Remove usage of getDOMNode

--- src/react-infinite.jsx
+++ src/react-infinite.jsx
@@ -142,7 +142,7 @@ var Infinite = React.createClass({
   },
 
   getScrollTop() {
-    return this.refs.scrollable.getDOMNode().scrollTop;
+    return this.refs.scrollable.scrollTop;
   },
 
   // Given the scrollTop of the container, computes the state the
@@ -164,11 +164,11 @@ var Infinite = React.createClass({
   },
 
   infiniteHandleScroll(e) {
-    if (e.target !== this.refs.scrollable.getDOMNode()) {
+    if (e.target !== this.refs.scrollable) {
       return;
     }
 
-    this.props.handleScroll(this.refs.scrollable.getDOMNode());
+    this.props.handleScroll(this.refs.scrollable);
     this.handleScroll(e.target.scrollTop);
   },
 
